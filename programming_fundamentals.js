07523beae6985b11ea8e399f98d74d5e1b6f4d88
mackwick <mackenziewicker@gmail.com>
2023-11-21 17:09:30 -0500

    Answered programming principles Qs

---
+++ programming_fundamentals.js
@@ -0,0 +1,37 @@
+//********************************** */
+// SECTION 1: PROGRAMMING FUNDAMENTALS
+//********************************** */
+
+//Read the linked article on programming principles, focus on the following nine:
+
+// DRY - If you find that you're repeating the same long chunk of code multiple times, stop and use a loop, function, variable, etc.
+
+// KISS - Avoid weird, complex code.
+
+// Avoid creating a YAGNI - Don't add unneccessary stuff you don't need.
+
+// Do the simplest thing that could possibly work - Look for the easiest option.
+
+// Don't make me think - Write code that is easy for future, other editors to understand.
+
+// Write code for the maintainer - Write code that is easy to fix and update.
+
+// Single responsibility principle - Every piece should do one, well-definied thing.
+
+// Avoid premature optimization - Focus on making your code work before making it work faster.
+
+// Separation of concerns - Keep the things that do different things in different places.
+
+//********************************** */
+// ANSWER
+//********************************** */
+
+// Write a ~1 sentence summary for each one 
+    // (see above)
+
+// Which ones surprise you (if any)?
+    // None. They make sense.
+
+// Which one is currently giving you the most struggle?
+    // I'm probably not doing the simplest thing that could possible work because I don't know enough.
+
