945427a22be64a1afa523348972bb2b1b9cb92ff
mackwick <mackenziewicker@gmail.com>
2023-11-22 16:14:06 -0500

    sovled vending machine and calculate callback function

--- combining_datatypes.js
+++ combining_datatypes.js
@@ -77,3 +77,62 @@ const powerButton = () => {
 }
 
 powerButton()()
+
+
+//***************************** */
+// MODEL A VENDING MACHINE
+//***************************** */
+
+// a vending machine is an object
+// it has an array of snacks (make 3 snacks)
+// snacks are objects that have a name and a price
+// a vending machine has a function vend that allows user to enter the array position (a number) of the snack and then that snack will be returned
+// Be able to call vendingMachine.vend() with a valid integer to return a snack
+
+const vendingMachine = {
+    snacks: ["Pumpkin Delights", "Popcorn", "Ooey Gooey Cake"],
+    vend(i) {
+        return vendingMachine.snacks[i]
+    },
+}
+
+console.log(vendingMachine.vend(0))
+
+
+//***************************** */
+// CALLBACKS
+//***************************** */
+
+// Make a function add that takes two arguments (numbers) and sums them together
+const add = (a, b) => {
+    return a+b
+}
+
+// Make a function subtract that takes two arguments (numbers) and subtracts them
+const subtract = (a, b) => {
+    return a-b
+}
+
+// Make a function multiply that takes two arguments and multiplies them
+const multiply = (a, b) => {
+    return a*b
+}
+
+// Make a function divide that takes two arguments and divides them
+const divide = (a, b) => {
+    return a/b
+}
+
+// Make a function calculate that takes three arguments. Assume the two arguments are a number ie num1, num2 and a function called operates (a callback).
+const calculate = (num1, num2, operates) => {
+    return operates(num1, num2)
+}
+
+// Make it so that when calculate is invoked, the callback "operates" on the numbers and returns the value. Call calculate 4 times, each time using one of the operation functions you wrote
+
+console.log(calculate(3, 3, add))
+console.log(calculate(3, 3, subtract))
+console.log(calculate(3, 3, multiply))
+console.log(calculate(3, 3, divide))
+
+// Function Definition Placement ************************* */
